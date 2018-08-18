dc04165453855e940ba7a1aca78c85485df51c71
Anani 2017 <miyiyangdezixin@163.com>
2018-08-19 02:21:17 +0800

    set clean-webpack-plugin & modified output path

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -13,7 +13,7 @@ module.exports = webpackMerge(webpackBaseConfig, {
         //
     },
     devServer: {
-        contentBase: path.join(__dirname, 'market'),
+        contentBase: path.join(__dirname, '../market'),
         port:8080,
         // host:'0.0.0.0'
     },
