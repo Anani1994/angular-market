ea668be2ec224121e49ad889036c5aab3711f763
Anani1994 <dongwhchn@163.com>
2018-09-01 23:50:24 +0800

    add angualr-ui-route

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
-// 引入通用 css
+// 引入通用 less
 import './styles/common/common.less';
-// 引入自定义的动画
+// 引入自定义动画的 less
 import './styles/animates/animates.less';
-// 引入 bootstrap 的 less 文件
-import 'bootstrap/bootstrap.less';
 // 引入angular
 import Angular from 'angular';
+// 引入路由 angular-route
+// import Route from 'angular-route';
+// 引入动画 angular-animate
+import animate from 'angular-animate';
+// 引入 angular-ui-bootstrap
+// import uiBootstrap from 'angular-ui-bootstrap/ui-bootstrap';
+// 引入 angular-ui-router
+import uiRoute from 'angular-ui-router';
 // 定义一个angular模块
-let App = Angular.module('app', [
-    require('angular-animate'),
-    require('angular-route')
-]);
+let App = Angular.module('app', [animate, uiRoute]);
 // 引入自定义的指令文件
 require('./templates/global-tips/global-tips.js')(App);
 // 引入路由
--- src/route/index.js
+++ src/route/index.js
@@ -1,12 +1,29 @@
 module.exports = function (App) {
-    App.config(['$routeProvider', function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                // 路径问题
-                // templateUrl: absolute/path 建议改成 template: require('relative/path')
-                template: require('../views/login/login.pug')
-            }).when('/index', {
-                template: require('../views/index/index.pug')
-        }).otherwise({redirectTo: '/index'});
+    // App.config(['$routeProvider', function ($routeProvider) {
+    //     $routeProvider
+    //         .when('/', {
+    //             // 路径问题
+    //             // templateUrl: absolute/path 建议改成 template: require('relative/path')
+    //             template: require('../views/jade/login/login.pug')
+    //         }).when('/index', {
+    //             template: require('../views/jade/index/index.pug')
+    //     }).otherwise({redirectTo: '/index'});
+    // }]);
+    App.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider
+            .state('app', {
+                url: '/',
+
+            })
+            .state('app.index', {
+                url: '/index',
+                templateUrl: '../views/jade/index/index.html'
+    
+            })
+            .state('app.login', {
+                url: '/login',
+                templateUrl: '../views/jade/login/login.html'
+    
+            });
     }]);
 }
